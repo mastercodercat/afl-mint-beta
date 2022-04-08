4698a770866642b6d60cdb1854ecfd9c22685054
taylor_animocalabs <smartgeek.taylor@gmail.com>
2022-04-09 01:10:53 +0900

    Updated CustomButton with size

--- src/components/Button/CustomButton.tsx
+++ src/components/Button/CustomButton.tsx
@@ -3,51 +3,64 @@ import { FormGroup } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 
-const useButtonStyles = makeStyles({
-  contained: {
-    '&': {
-      backgroundImage:
-        'linear-gradient(264.12deg, #7381FC 9.33%, #086BDF 90.67%)',
-      borderRadius: '2px',
-      color: '#fff',
-      fontFamily: 'PODIUMSharp-49',
-      fontSize: '16px',
-      fontWeight: 'bold',
-      letterSpacing: '0.1em',
-      padding: '10px'
+const useButtonStyles = (fontColor: string, size: string) =>
+  makeStyles({
+    primary: {
+      '&': {
+        backgroundImage:
+          'linear-gradient(264.12deg, #7381FC 9.33%, #086BDF 90.67%)',
+        borderRadius: '2px',
+        color: '#fff',
+        fontFamily: 'PODIUMSharp-49',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        letterSpacing: '0.1em'
+      },
+      '&:hover': {
+        boxShadow: '0px 5px 15px rgba(40, 114, 232, 0.4)'
+      },
+      '&:active': {
+        background: 'linear-gradient(264.12deg, #7381FC 9.33%, #086BDF 90.67%)'
+      }
     },
-    '&:hover': {
-      boxShadow: '0px 5px 15px rgba(40, 114, 232, 0.4)'
+    secondary: {
+      '&': {
+        borderColor: '#fff',
+        borderRadius: '2px',
+        color: '#fff',
+        fontFamily: 'PODIUMSharp-49',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        letterSpacing: '0.1em'
+      },
+      '&:hover': {}
     },
-    '&:active': {
-      background: 'linear-gradient(264.12deg, #7381FC 9.33%, #086BDF 90.67%)'
+    text: {
+      '&': {
+        color: fontColor,
+        fontFamily: 'PODIUMSharp-49',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        letterSpacing: '0.1em'
+      },
+      '&:hover': {}
+    },
+    small: {
+      '&': {
+        padding: '7px'
+      }
+    },
+    medium: {
+      '&': {
+        padding: '7px'
+      }
+    },
+    large: {
+      '&': {
+        padding: '10px'
+      }
     }
-  },
-  outlined: {
-    '&': {
-      borderColor: '#fff',
-      borderRadius: '2px',
-      color: '#fff',
-      fontFamily: 'PODIUMSharp-49',
-      fontSize: '16px',
-      fontWeight: 'bold',
-      letterSpacing: '0.1em',
-      padding: '10px'
-    },
-    '&:hover': {}
-  },
-  text: {
-    '&': {
-      color: '#fff',
-      fontFamily: 'PODIUMSharp-49',
-      fontSize: '16px',
-      fontWeight: 'bold',
-      letterSpacing: '0.1em',
-      padding: '10px'
-    },
-    '&:hover': {}
-  }
-})
+  })
 
 const theme = createTheme({
   palette: {
@@ -55,31 +68,35 @@ const theme = createTheme({
   }
 })
 
-export type CustomButtonProps = Omit<ButtonProps, 'variant'> & {
-  variant: 'contained' | 'outlined' | 'text'
-  label: string
-  className?: string
-  fontColor?: string
-  isDisabled?: boolean
-  onClick?: () => {}
-}
+export type CustomButtonProps = ButtonProps<
+  'button',
+  {
+    type: 'primary' | 'secondary' | 'text'
+    label: string
+    className?: string
+    fontColor?: string
+    onClick?: () => {}
+    size?: string
+  }
+>
 
 const CustomButton = ({
-  variant,
+  type,
   label,
   className,
   fontColor,
   onClick,
+  size,
   ...rest
 }: CustomButtonProps) => {
-  const useButtonClasses = useButtonStyles()
+  const classes = useButtonStyles(fontColor || '', size || '')()
 
   return (
     <ThemeProvider theme={theme}>
       <FormGroup>
         <Button
-          variant={variant}
-          className={useButtonClasses[variant]}
+          className={`${classes[type]} ${classes[size || 'large']}`}
+          onClick={() => {}}
           {...rest}
         >
           {label}
